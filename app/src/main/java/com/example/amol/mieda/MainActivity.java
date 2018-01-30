a961a06724e65e99761cb0e2626a45c0faa69b7a
frextrite <29180487+frextrite@users.noreply.github.com>
2018-01-30 19:21:21 +0530

    Removing Unused Code

--- app/src/main/java/com/example/amol/mieda/MainActivity.java
+++ app/src/main/java/com/example/amol/mieda/MainActivity.java
@@ -82,15 +82,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
