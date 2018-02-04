21dfdac188d10a01eb23b760e580b710abda548a
frextrite <29180487+frextrite@users.noreply.github.com>
2018-02-04 23:37:13 +0530

    Cleaning Code

--- app/src/main/java/com/example/amol/mieda/CameraActivity.java
+++ app/src/main/java/com/example/amol/mieda/CameraActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.wonderkiln.camerakit.CameraKit;
